53127fd7f27063db264454a81d2ce26ebe04448f
isemang <a55306039@gmail.com>
2021-11-02 23:01:15 +0900

    [2021.11.02] 박민진 - citizen project 최초 생성

---
+++ src/main/java/com/story/citizen/CitizenServerApplication.java
@@ -0,0 +1,13 @@
+package com.story.citizen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizenServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/story/citizen/CitizenServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.story.citizen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
