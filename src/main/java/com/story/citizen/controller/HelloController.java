edb6ab8a7f4c7f3d9c103ca6d3b598cd413db48a
isemang <a55306039@gmail.com>
2021-11-25 18:34:33 +0900

    feat : hello-test/ 테스트용 POST Api 추가

--- src/main/java/com/story/citizen/controller/HelloController.java
+++ src/main/java/com/story/citizen/controller/HelloController.java
@@ -1,17 +1,37 @@
 package com.story.citizen.controller;
 
 import com.story.citizen.dto.Hello;
+import com.story.citizen.dto.response.Response;
+import com.story.citizen.dto.response.ResponseCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+
+    private final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @ResponseBody
+    @PostMapping("hello-test")
+    public ResponseEntity<String> helloPostTest(@RequestBody Hello hello) {
+        logger.info("*******수신 Data::" + hello.toString());
+        Response response = Response.of(ResponseCode.RP000);
+        ResponseEntity<String> responseEntity = new ResponseEntity<>(response.getResponseResult(), HttpStatus.OK);
+
+        return responseEntity;
     }
 }
--- src/main/java/com/story/citizen/dto/Hello.java
+++ src/main/java/com/story/citizen/dto/Hello.java
@@ -1,41 +1,15 @@
 package com.story.citizen.dto;
 
-//빌더 패턴으로 구성한 DTO
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Hello {
-    private final String id;
-    private final String name;
-    private final String birthDate;
-    private final String address;
-
-    public static class Builder {
-        //필수 매개변수
-        private final String id;
-        private final String name;
-        private final String birthDate;
-
-        //선택 매개변수 - 기본값으로 초기화
-        private String address = "";
-
-        public Builder(String id, String name, String birthDate) {
-            this.id = id;
-            this.name = name;
-            this.birthDate = birthDate;
-        }
-
-        public Builder address(String val) {
-            address = val;
-            return this;
-        }
-
-        public Hello build() {
-            return new Hello(this);
-        }
-    }
-
-    private Hello(Builder builder) {
-        id = builder.id;
-        name = builder.name;
-        birthDate = builder.birthDate;
-        address = builder.address;
-    }
+    private String id;
+    private String name;
+    private String birthDate;
+    private String address;
 }
